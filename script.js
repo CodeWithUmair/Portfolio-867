b0b29d03cfeff2afb59e5343a080bf3386c293bc
Umair <codewithumair867@gmail.com>
2022-08-19 02:50:42 +0500

    preloader add

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const fadeOut = () => {
+  const loaderWrapper = document.querySelector("#wrapper");
+  loaderWrapper.classList.add("fade");
+};
+window.addEventListener("load", fadeOut);
 $(document).ready(function () {
   $(window).scroll(function () {
     // sticky navbar on scroll script
